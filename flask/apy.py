adf707a0eb73c44aef4dbf289221629e08cdf90f
joeljohn <joel.regalado.morales@gmail.com>
2019-02-01 01:42:43 -0500

    Section 5, class 69

--- flask/apy.py
+++ flask/apy.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import  Api
+from flask_jwt import JWT
 from security import autenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'joel'
@@ -9,59 +11,9 @@ api = Api(app)
 
 jwt = JWT(app, autenticate, identity)
 
-
-items = []
-
-class Item(Resource):
-
-    parse = reqparse.RequestParser()
-    parse.add_argument('price',
-            type=float,
-            required=True,
-            help='This field cannot be left blank!'
-    )
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x : x['name'] == name, items),None)
-        return {'item' : item }, 201 if item  else 404
-    
-    def post(self, name ):
-        if next(filter(lambda x : x['name'] == name, items),None) is not None:
-            return {'message' : "An item with name {} already exists".format(name)}, 400
-        data = Item.parse.parse_args()
-        item = {
-                'name' : name , 
-                'price' : data['price'
-                ]}
-        items.append(item)
-        return item, 201
-    
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name , items))
-        return {'message' : 'Items was delete : "{}"'.format(name)}, 201
-    
-    def put(self, name):
-        data = Item.parse.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = { 
-                'name' : name,
-                'price' : data['price']
-            }
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items' : items}
-
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList,"/items")
+api.add_resource(UserRegister,"/register")
 
 app.run(debug=True)
 
